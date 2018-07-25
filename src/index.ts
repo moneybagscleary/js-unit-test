a0aa92d27f545592845c91740ee3ac633856fdc5
Christopher Cleary <christopher_cleary@clinicalarchitecture.com>
2018-07-25 12:47:02 -0400

    stubbing out create function for creating test or suite

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as program from 'commander';
 
 import { runTest, runSuite, suites } from './command/run'
 import { init } from './command/init';
+import { create } from './command/create';
 
 program
     .command('init')
@@ -24,6 +25,13 @@ program
     .command('suites')
     .description('List the configured test suites')
     .action(suites);
+
+program
+    .command('create [name] [description]')
+    .description('Creates a specified test or test suite')
+    .option('-s, --suite', 'Creates a suite with the specified name and description')
+    .option('-t, --test', 'Creates a new test with the specified name and description')
+    .action(create);
 
 program
     .version('1.0.0')
